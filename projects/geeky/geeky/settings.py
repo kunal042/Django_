fe16155ade47ebfeea84afda53d847b8c9445ab4
kunal kumar <k1509kunal@gmail.com>
2024-03-08 16:01:49 +0530

    add app in Django

--- projects/geeky/geeky/settings.py
+++ projects/geeky/geeky/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "course",
-]
+    "fee",
+    "result",
+    ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
