0d1ae5b286bba9ccf3c4ec9ada83a25d0b7d76e7
kunal kumar <k1509kunal@gmail.com>
2024-03-09 17:40:49 +0530

    views

--- projects/geeky/course/views.py
+++ projects/geeky/course/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def course_home(request):
-    s = "This is the course page "
-    return HttpResponse(s)
+    
+    return render(request, "index.html", {"name":"kunal", "age":22, "city":"Gurgaon"})
