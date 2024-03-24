4aaf5747562514f10f9c3483a3e41fe9e6abbf6f
kunal kumar <k1509kunal@gmail.com>
2024-03-24 09:20:26 +0530

    form

--- projects/djangoform/mainApp/forms.py
+++ projects/djangoform/mainApp/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 
+
 class StudentRegistration(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(
+        label='Your Name', 
+        label_suffix=" ", 
+        initial='sonma', 
+        required=False, 
+        help_text="limit 30 char")
     email = forms.EmailField()
-    
+    first_name = forms.CharField()
+    mobile = forms.IntegerField()
+
+    key = forms.CharField(widget=forms.HiddenInput())
--- projects/djangoform/mainApp/views.py
+++ projects/djangoform/mainApp/views.py
@@ -3,5 +3,7 @@ from .forms import StudentRegistration
 
 
 def showformdata(request):
-    fm = StudentRegistration()
-    return render(request, 'mainapp/form.html', {"form":fm})
+    fm = StudentRegistration(auto_id=True, label_suffix="  ", initial={
+                             "name": "Kunal", 'email': "kunal@outlook.com"})
+    fm.order_fields(field_order=['first_name', 'name', 'email'])
+    return render(request, 'mainapp/form.html', {"form": fm})
