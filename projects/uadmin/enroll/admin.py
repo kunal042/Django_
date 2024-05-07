ac5e661f17e0f208941d68fe4c34f5bf1b9a59b5
kunal kumar <k1509kunal@gmail.com>
2024-05-07 13:55:18 +0530

    admin

---
+++ projects/uadmin/enroll/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .form import SignUpForm, EditUserProfileForm
+
+# Register your models here.
+# @admin.register(SignUpForm)
+# class ShowData(admin.ModelAdmin):
+#     list_display = ["name","email" ]
