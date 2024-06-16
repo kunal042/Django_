3ce5d00c88f3f0473e4f060a90fbe59bc8791c01
Kunal <kunalroy199915@gmail.com>
2024-06-16 17:30:01 +0530

    super user admin

--- projects/ORM/gs1/enroll/views.py
+++ projects/ORM/gs1/enroll/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+def studentinfo(request):
+
+    stud = Student.objects.all()
+    print(stud)
+
+    return render(request , 'enroll/base.html', {"stud":stud})
+
+
